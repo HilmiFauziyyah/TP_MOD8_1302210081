cd42a00e1d17fe685254ad4b25cd572a5518f30a
hilmifauziyyah <hilmifs2113@gmail.com>
2023-04-09 23:17:02 +0700

    menyelesaikan program dengan runtime configuration

--- TP_MOD8_1302210081/TP_MOD8_1302210081/Program.cs
+++ TP_MOD8_1302210081/TP_MOD8_1302210081/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static TP_MOD8_1302210081.CovidConfig;
+
+readWrite rw = new readWrite();
+rw.UbahDerajat();
+
+Console.WriteLine("Berapa suhu badan anda pada saat ini? Dalam nilai " + rw.config.satuan_suhu);
+Console.WriteLine("(celcius 36,5 - 37,5 atau 97.7 - 99,5)");
+double suhu = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+int hari = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if ((rw.config.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) && hari < rw.config.batas_hari_demam)
+    {
+    Console.WriteLine(rw.config.pesan_diterima);
+    }
+    else if ((rw.config.satuan_suhu == "farenheit") && suhu >= 97.7 && suhu <= 99.5 && hari < rw.config.batas_hari_demam)
+    {
+    Console.WriteLine(rw.config.pesan_diterima);
+    }
+    else
+    {
+    Console.WriteLine(rw.config.pesan_ditolak);
+    }
